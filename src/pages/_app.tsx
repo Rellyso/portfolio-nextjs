6ef30e8b2c88d2d6781c25bca8cb743c058ef8e6
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-04-08 13:57:09 -0300

    Added prismic provider and fetching data at index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,28 @@
+import { PrismicProvider } from '@prismicio/react';
+import Link from 'next/link';
 import { ThemeProvider } from 'styled-components';
+import { linkResolver } from '../services/prismic';
 
 import GlobalStyles from '../styles/global';
 import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>
+            {children}
+          </a>
+        </Link>
+      )}
+    >
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </PrismicProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from 'next';
 import { ContactForm } from '../components/ContactForm';
 import { Experiences } from '../components/Experiences';
 import { Footer } from '../components/Footer';
@@ -5,6 +6,7 @@ import { Header } from '../components/Header';
 import { HomeHero } from '../components/HomeHero';
 import { Knowledge } from '../components/Knowledge';
 import { Projects } from '../components/Projects';
+import { getPrismicClient } from '../services/prismic';
 import { HomeContainer } from '../styles/HomeStyles';
 
 export default function Home() {
@@ -23,4 +25,16 @@ export default function Home() {
       <Footer />
     </HomeContainer>
   );
+}
+
+export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+  const prismic = getPrismicClient({ previewData });
+
+  const projectResponse = await prismic.getAllByType('projeto')
+
+  console.log(projectResponse)
+
+  return {
+    props: {}
+  }
 }
